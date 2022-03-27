9323436dc1dd97d65c88075091a4579bc344d827
sravigowda <sravigowda@gmail.com>
2022-03-27 18:46:09 +0530

    Update non_repeat_substring.py
    Adding another method using sliding windows technique

--- non_repeat_substring.py
+++ non_repeat_substring.py
@@ -10,11 +10,28 @@ def non_repeat_substring(s1):
             char_count = {s1[index]: 1}
     return max(max_length, len(char_count))
 
+# Following solution is using sliding windows technique
+def no_repeat_substring(s1):
+    max_length = 0
+    char_index = {}
+    window_start = 0
+    for window_end in range(len(s1)):
+        if s1[window_end] in char_index:
+            window_start = max(window_start, char_index[s1[window_end]] + 1)
+        char_index[s1[window_end]] = window_end
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
+
 
 def main():
     print("Length of the longest substring: " + str(non_repeat_substring("aabccbb")))
     print("Length of the longest substring: " + str(non_repeat_substring("abbbb")))
     print("Length of the longest substring: " + str(non_repeat_substring("abccdefc")))
+    
+    print("Length of the longest substring: " + str(no_repeat_substring("aabccbb")))
+    print("Length of the longest substring: " + str(no_repeat_substring("abbbb")))
+    print("Length of the longest substring: " + str(no_repeat_substring("abccdefc")))
+
 
 
 main()
